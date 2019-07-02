0eea4477d4b12ed4a6508d87b843076dfb4c68a9
DrewBoiii <goodchickenboy@mail.ru>
2019-07-02 13:47:49 +0400

    fix request syntax, add mysql driver to lib, doesn't work yet

--- src/dao/SignUpDAO.java
+++ src/dao/SignUpDAO.java
@@ -4,20 +4,21 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import data.entities.User;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class SignUpDAO {
     public static Connection getConnection() throws SQLException {
         MysqlDataSource dataSource = new MysqlDataSource();
-        String url = "jdbc:mysql:127.0.0.1:3306/word_counter_app";
+        String url = "jdbc:mysql://127.0.0.1:3306/word_counter_app";
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
             dataSource.setUrl(url);
             dataSource.setUser("root");
             dataSource.setPassword("");
-            //DriverManager.getConnection(url, "root", "");
+//            DriverManager.getConnection(url, "root", "");
         } catch (Exception e){
             e.printStackTrace();
         }
@@ -27,10 +28,11 @@ public class SignUpDAO {
 
     public static int saveUser(User user){
         int status = 0;
+        Connection connection = null;
 
         try {
-            Connection connection = getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("insert into user values (?, ?);");
+            connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (nickname, password) VALUES (?,?)");
 
             preparedStatement.setString(1, user.getNickname());
             preparedStatement.setString(2, user.getPassword());
@@ -39,6 +41,14 @@ public class SignUpDAO {
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if(connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
         return status;
